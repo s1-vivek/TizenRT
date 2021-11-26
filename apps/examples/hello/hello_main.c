a42731383fd1f5a6459094ef27440f0ab3aa0336
s1.vivek <s1.vivek@samsung.com>
2021-11-26 11:34:10 +0530

    apps/examples/hello: Implement basic UDP data transfer to check failure in sending data.
    Follow these steps for checking failure
    Board 1:
    1) Start wifi on board 1
       wm_test start
    2) Set board 1 into SoftAP mode
       wm_test softap tp1x 12341234
    3) Run UDP Server using:
       hello 0 [Data Size to send]
       ex- hello 0 100 (it will recieve and then send 100 bytes of data).
    
    Board 2:
    1) Start wifi on board 2
       wm_test start
    2) Join board 2 to softap created on board 1
       wm_test join tp1x 12341234
    3) Run UDP client using:
       hello 1 [Data size to send]
       ex- hello 1 100 (it will send and then receive 100 bytes of data).

--- apps/examples/hello/hello_main.c
+++ apps/examples/hello/hello_main.c
@@ -56,6 +56,241 @@
 
 #include <tinyara/config.h>
 #include <stdio.h>
+#include<string.h> 
+#include<stdlib.h> 
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include "../wifi_manager/wm_test/wm_test_log.h"
+
+#define SERVER_IP CONFIG_LWIP_DHCPS_SERVER_IP
+#define SERVER_PORT 8788
+#define WT_BUF_SIZE 1024
+#define TAG "[Hello]"
+#define WT_ACK_SIZE 128
+
+static int _recv_data_udp(int fd, int size, struct sockaddr_in *caddr) 
+{
+	char buf[WT_BUF_SIZE];
+	int remain = size;
+	unsigned int clen = sizeof(struct sockaddr_in);
+	int cnt = 1;
+	int total_received = 0;
+	int total_ack_for_received = 0;
+	WT_LOG(TAG, "_recv_data_udp start");
+	while (remain > 0) {
+		int read_size = remain > WT_BUF_SIZE ? WT_BUF_SIZE : remain;
+		int nbytes = recvfrom(fd, buf, read_size, 0, (struct sockaddr*)caddr, &clen);
+		if (nbytes == 0) {
+			WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_received);
+			WT_LOGE(TAG, "connection closed");
+			return -1;
+		} else if (nbytes < 0) {
+			if (errno == EWOULDBLOCK) {
+				WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_received);
+				WT_LOGE(TAG, "timeout error %d", errno);
+				return -1;
+			} else {
+				WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_received);
+				WT_LOGE(TAG, "connection error %d", errno);
+				return -1;
+			}
+		}
+		int nack;
+		if(nbytes == WT_BUF_SIZE) {
+			nack = sendto(fd, buf, WT_ACK_SIZE, 0, (struct sockaddr*)caddr, clen);
+			if (nack == 0) {
+				WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_ack_for_received);
+				WT_LOGE(TAG, "connection closed");
+				return -1;
+			} else if (nack < 0) {
+				if (errno == EWOULDBLOCK) {
+					WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_ack_for_received);
+					WT_LOGE(TAG, "timeout error %d", errno);
+					return -1;
+				} else {
+					WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_ack_for_received);
+					WT_LOGE(TAG, "connection error %d", errno);
+					return -1;
+				}
+			}
+			total_ack_for_received += nbytes;
+		}
+		total_received += nbytes;
+		remain -= nbytes;
+		//WT_LOG(TAG, "Packet Number: %d Received: %d Total Received: %d Remaining: %d", cnt, nbytes, total_received, remain);
+		cnt++;
+	}
+	WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d and total ack for %d received data", total_received, total_ack_for_received);
+	return 0;
+}
+
+static int _send_data_udp(int fd, int size, struct sockaddr_in *caddr)
+{
+	char buf[WT_BUF_SIZE];
+	int remain = size;
+	unsigned int clen = sizeof( struct sockaddr_in);
+	int cnt = 1;
+	int total_sent = 0; 
+	int total_ack_for_sent = 0;
+	WT_LOG(TAG, "_send_data_udp start");
+	while (remain > 0) {
+		int send_size = remain > WT_BUF_SIZE ? WT_BUF_SIZE : remain;
+		int nbytes = sendto(fd, buf, send_size, 0, (struct sockaddr*)caddr, clen);
+		if (nbytes == 0) {
+			WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_sent);
+			WT_LOGE(TAG, "connection closed");
+			return -1;
+		} else if (nbytes < 0) {
+			if (errno == EWOULDBLOCK) {
+				WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_sent);
+				WT_LOGE(TAG, "timeout error %d", errno);
+				return -1;
+			} else {
+				WT_LOG(TAG, "TOTAL BYTES SEND: %d", total_sent);
+				WT_LOGE(TAG, "connection error %d", errno);
+				return -1;
+			}
+		}
+		int nack;
+		if(nbytes == WT_BUF_SIZE) {
+			nack= recvfrom(fd, buf, WT_ACK_SIZE, 0, (struct sockaddr*)caddr, &clen);
+			if (nack == 0) {
+				WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_ack_for_sent);
+				WT_LOGE(TAG, "connection closed");
+				return -1;
+			} else if (nack < 0) {
+				if (errno == EWOULDBLOCK) {
+					WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_ack_for_sent);
+					WT_LOGE(TAG, "timeout error %d", errno);
+					return -1;
+				} else {
+					WT_LOG(TAG, "TOTAL BYTES RECEIVED: %d", total_ack_for_sent);
+					WT_LOGE(TAG, "connection error %d", errno);
+					return -1;
+				}
+			}
+			total_ack_for_sent += nbytes;
+		}
+		total_sent += nbytes;
+		remain -= nbytes;
+		//WT_LOG(TAG, "Packet Number: %d Sent: %d Total Sent: %d Remaining: %d", cnt, nbytes, total_sent, remain);
+		cnt++;
+	}
+	WT_LOG(TAG, "TOTAL BYTES SEND: %d and total ack for %d send data", total_sent, total_ack_for_sent);
+	return 0;
+}
+
+static int _udp_server(int udp_data) {
+	struct sockaddr_in saddr, caddr;
+	int sockfd = 0;
+	int ret = 0;
+
+	/* Socket Creation */
+	WT_LOG(TAG, "create socket");
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		WT_LOGE(TAG, "create socket fail %d", errno);
+		return -1;
+	}
+
+	/* Set Timeout for Sockets data transfer */
+	struct timeval tv;
+	tv.tv_sec = 30;
+	tv.tv_usec = 0;
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+	
+	/* Socket binding process */
+	WT_LOG(TAG, "bind INADDR_ANY PORT:%d", SERVER_PORT);
+	memset(&caddr, 0, sizeof(caddr));
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	saddr.sin_port = htons(SERVER_PORT);
+	ret = bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr));
+	if (ret < 0) {
+		WT_LOGE(TAG, "bind fail %d", errno);
+		return -1;
+	}
+	
+	unsigned int clen = sizeof(struct sockaddr_in);
+	int slen = sizeof(struct sockaddr_in);
+	
+	memset(&caddr, 0, sizeof(caddr));
+	/* Receive data through udp socket */
+	ret = _recv_data_udp(sockfd, udp_data, &caddr);
+	if (ret < 0) {
+		WT_LOGE(TAG, "recv fail size %d ret %d code %d", udp_data, ret, errno);
+		close(sockfd);
+		return -1;
+	}
+	WT_LOG(TAG, "_recv_data_udp OK");
+	
+	/* Send data through udp socket */
+	ret = _send_data_udp(sockfd, udp_data , &caddr);
+	if (ret < 0) {
+		WT_LOGE(TAG, "send fail size %d ret %d code %d", udp_data, ret, errno);
+		close(sockfd);
+		return -1;
+	}
+	WT_LOG(TAG, "_send_data_udp OK");
+	
+	close(sockfd);
+	WT_LOG(TAG, "terminate udp data transfer");
+	return 0;
+}
+
+int _udp_client(int size)
+{
+	struct sockaddr_in saddr;
+	int ret = 0;
+	int sockfd = 0;
+	
+	/* Socket Creation */
+	WT_LOG(TAG, "create socket");
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		WT_LOGE(TAG, "create socket fail %d", errno);
+		return -1;
+	}
+	
+	/* Set Timeout for Sockets data transfer */
+	struct timeval tv;
+	tv.tv_sec = 30;
+	tv.tv_usec = 0;
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+	/* Connect the socket to the server */
+	WT_LOG(TAG, "connect to %s:%d", SERVER_IP, SERVER_PORT);
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(SERVER_PORT);
+	inet_aton(SERVER_IP, &(saddr.sin_addr));
+	
+	unsigned int clen = sizeof(struct sockaddr_in);
+	int slen = sizeof(struct sockaddr_in);
+	/* Send data through udp socket */
+	ret = _send_data_udp(sockfd, size, &saddr);
+	if (ret < 0) {
+		WT_LOGE(TAG, "send fail size %d ret %d code %d", size, ret, errno);
+		close(sockfd);
+		return -1;
+	}
+	WT_LOG(TAG, "_udp_send_data OK");
+
+	
+	/* Receive data through udp socket */
+	ret = _recv_data_udp(sockfd, size, &saddr);
+	if (ret < 0) {
+		WT_LOGE(TAG, "receive fail size %d ret %d code %d", size, ret, errno);
+		close(sockfd);
+		return -1;
+	}
+	WT_LOG(TAG, "_udp_recv_data OK");
+	
+	close(sockfd);
+	WT_LOG(TAG, "udp data transmission finished");
+	return 0;
+}
+
 
 /****************************************************************************
  * hello_main
@@ -67,6 +302,16 @@ int main(int argc, FAR char *argv[])
 int hello_main(int argc, char *argv[])
 #endif
 {
-	printf("Hello, World!!\n");
+	if(argc == 1) {
+		printf("Hello, World!!\n");
+	} else {
+		int opt = atoi(argv[1]);
+		int data = atoi(argv[2]);
+	        if (opt == 0) {
+			_udp_server(data);
+		} else {
+			_udp_client(data);
+		}	
+	}	
 	return 0;
 }
